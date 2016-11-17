48c6da0c1776069bf2c88392af6a1408a998a948
Pavel Zhukov <cray0000@gmail.com>
2016-11-17 13:28:29 +0100

    Move head before styles

--- server/defaultClientLayout.js
+++ server/defaultClientLayout.js
@@ -2,8 +2,8 @@ module.exports = (p) => `
 <html>
   <head>
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    ${p.styles || ''}
-    ${p.head || ''}
+    ${p.head || ''}    
+    ${p.styles || ''}    
     <script>window.IS_REACT = true</script>
   </head>
   <body>
