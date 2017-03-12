eeff492c861a7fc66a58d6b66120143738dcdb06
Pavel Zhukov <cray0000@gmail.com>
2017-03-12 15:16:44 +0100

    Style the global loading indicator (when the page awaits to be rendered on the client side)

--- server/defaultClientLayout.js
+++ server/defaultClientLayout.js
@@ -1,8 +1,13 @@
+const fs = require('fs')
+const path = require('path')
+const defaultStyles = fs.readFileSync(path.join(__dirname, 'defaultStyles.css'), 'utf8')
+
 module.exports = (p) => `
 <html>
   <head>
     <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-    ${p.head || ''}    
+    ${p.head || ''}
+    <style>${defaultStyles}</style>
     ${p.styles || ''}    
     <script>window.IS_REACT = true</script>
   </head>
